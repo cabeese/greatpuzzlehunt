cbd9f7aecfe96726f67a6fdd12dfa5ac2eb598d5
Noah <Noah.Strong+github@outlook.com>
2019-12-15 15:04:42 -0600

    /tickets API: remove debugging and prep for prod

--- server/webhooks/tickets.js
+++ server/webhooks/tickets.js
@@ -15,6 +15,32 @@ PostRoute.route('/api/tickets', function(params, req, res, next) {
   handleRequest(req, res, req_token, req.body);
 });
 
+const handleRequest = async (req, res, req_token, body) => {
+  Meteor.logger.info("Logging raw transaction");
+  Meteor.logger.info(body);
+  res.setHeader('Content-Type', 'application/json');
+  /* Validate Token */
+  if (!req_token || req_token !== token) {
+    Meteor.logger.info(`Request on "/api/tickets" with BAD TOKEN: "${req_token}" from ${Meteor.logger.jstring(req.headers)}`);
+    res.statusCode = 403;
+    res.write('{ "error": "Invalid token" }');
+    return res.end();
+  }
+
+  /* Parse request and formulate response */
+  try {
+    let txData = convertCashnet(body);
+    await processTransaction(txData);
+    res.statusCode = 200;
+    res.end();
+  } catch (err) {
+    Meteor.logger.error("Failed to convert or process CashNet request");
+    Meteor.logger.error(err);
+    const msg = "Unable to parse request";
+    return badResponse(res, msg);
+  }
+};
+
 const badResponse = (res, message) => {
   Meteor.logger.info(`Request on "/api/tickets" with bad data`);
   body = {
@@ -27,31 +53,3 @@ const badResponse = (res, message) => {
   res.write(JSON.stringify(body));
   return res.end();
 }
-
-const handleRequest = async (req, res, req_token, body) => {
-  Meteor.logger.info(body); /* temp, though perhaps we should save these to the DB? */
-  /* Validate Token */
-  if (!req_token || req_token !== token) {
-    Meteor.logger.info(`Request on "/api/tickets" with BAD TOKEN: "${req_token}" from ${Meteor.logger.jstring(req.headers)}`);
-    res.setHeader('Content-Type', 'application/json');
-    res.statusCode = 403;
-    res.write('{ "error": "Invalid token" }');
-    return res.end();
-  }
-
-  /* Parse request and formulate response */
-  res.setHeader('Content-Type', 'application/json');
-
-  // Meteor.logger.info(`Request on "/api/tickets" from ${Meteor.logger.jstring(req.headers)}`);
-  try {
-    let txData = convertCashnet(body);
-    await processTransaction(txData);
-    res.statusCode = 200;
-    res.end();
-  } catch (err) {
-    Meteor.logger.error("Failed to convert or process CashNet request");
-    Meteor.logger.error(err);
-    const msg = err.message || "Unknown";
-    return badResponse(res, msg); /* TEMP - Prob shouldn't expose internal errors! */
-  }
-};
