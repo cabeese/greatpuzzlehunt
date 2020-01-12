ddb5b21cdca3bed1ece8c491491b906fcbed75a1
Noah <Noah.Strong+github@outlook.com>
2020-01-11 20:49:55 -0600

    Use later/SyncedCron to send nightly reports

--- server/imports/autoSendReports.js
+++ server/imports/autoSendReports.js
@@ -3,36 +3,26 @@ import moment from 'moment';
 
 import { sendReports } from '../../lib/imports/sendReports';
 
-const CHECK_INTERVAL = {
-  minutes: 5,
-};
+SyncedCron.add({
+  name: 'Nightly gear reports',
+  schedule: function(parser) {
+    // We're using UTC, so 8:15 = 12:15am PT
+    return parser.text('at 08:15am');
+  },
+  job: function() {
+    const gameState = Gamestate.findOne();
+    const sendTime = moment();
+    if(!gameState.doSendNightlyReports){
+      return;
+    }
 
-function autoSendReports() {
-  const now = moment();
-  const gameState = Gamestate.findOne();
-  if(!gameState.doSendNightlyReports){
-    return;
-  }
-  const lastAutoReportSend = moment(gameState.lastAutoReportSend || moment(now).subtract(2, 'day'));
-  const waitingUntil = moment(lastAutoReportSend).add(1, 'day');
-
-  Meteor.logger.info(`Checking to send auto reports. now: ${now} .. waiting for ${waitingUntil}`);
-
-  if (gameState.sendReportsTo.length <= 0) {
-    Meteor.logger.info("Skipping admin reports because \"gameState.sendReportsTo\" is empty!");
-    return;
-  }
-
-  if (now.isAfter(waitingUntil)) {
-    const sendTime = moment(now).startOf('hour');
     Meteor.logger.info(`Sending auto reports at ${sendTime} to: ${gameState.sendReportsTo}`);
     sendReports(gameState.sendReportsTo);
     Gamestate.update({ _id: gameState._id}, { $set: { lastAutoReportSend: sendTime }});
   }
-}
-
-Meteor.startup(() => {
-  // On Startup, init Interval for puzzle timeout watcher.
-  const interval = moment.duration(CHECK_INTERVAL).asMilliseconds();
-  Meteor.setInterval(autoSendReports, interval);
 });
+
+SyncedCron.config({
+  utc: true,
+});
+SyncedCron.start();
