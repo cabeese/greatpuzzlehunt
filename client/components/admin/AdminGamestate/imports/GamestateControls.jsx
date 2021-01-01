033f1a9575526c2a63de5ae5608209b87fde6048
cabeese <Noah.Strong+github@outlook.com>
2021-01-01 11:57:10 -0600

    admin gamestate: add controls to dynamically modify sendReportsTo

--- client/components/admin/AdminGamestate/imports/GamestateControls.jsx
+++ client/components/admin/AdminGamestate/imports/GamestateControls.jsx
@@ -1,6 +1,6 @@
 import { Meteor } from 'meteor/meteor';
 import React, { Component, PropTypes } from 'react';
-import { Radio, Form, Button, Header } from 'semantic-ui-react';
+import { Radio, Form, Button, Header, List } from 'semantic-ui-react';
 
 import GamestateComp from '../../../imports/GamestateComp';
 
@@ -10,6 +10,7 @@ class GamestateControlsInner extends Component {
     super(props);
     this.state = {
       reportDLBusy: false,
+      reportEmail: "",
     };
     this._getReport = this._getReport.bind(this);
   }
@@ -19,9 +20,12 @@ class GamestateControlsInner extends Component {
       this.setState({
         registration: props.gamestate.registration,
         gameplay: props.gamestate.gameplay,
+        sendReportsTo: props.gamestate.sendReportsTo,
       });
     }
   }
+
+  handleChange = (e, { name, value }) => this.setState({ [name]: value })
 
   render() {
     if (this.props.ready) {
@@ -32,7 +36,7 @@ class GamestateControlsInner extends Component {
     }
   }
 
-  _getReport(index) {
+  _getReport(e, index) {
     let self = this;
     this.setState({reportDLBusy: true});
     Meteor.call('admin.downloadReport', index, (error, result) => {
@@ -54,13 +58,6 @@ class GamestateControlsInner extends Component {
     return (
       <Form onSubmit={ (e) => e.preventDefault() }>
         <Header as="h3" content="Emails and Reports" />
-        <Form.Group>
-          <Form.Button icon="mail" content="Email (all 3) Reports to Me" onClick={(e) => Meteor.call('admin.sendReport')}/>
-        </Form.Group>
-
-        <Form.Group>
-          <Form.Button icon="mail" content="Email List of Users & Teams to Me" onClick={(e) => { Meteor.call('admin.sendUsersAndTeams'); alert("Emails are sending!"); }}/>
-        </Form.Group>
 
         <Header as="h4" content="Generate and Download Reports" />
         <Form.Group>
@@ -69,6 +66,42 @@ class GamestateControlsInner extends Component {
           { this._reportDownloadButton("Gear Orders", 2) }
         </Form.Group>
 
+        <Header as="h4" content="Email Reports" />
+        <Form.Group>
+          <Form.Button icon="mail" content="Email (all 3) Reports to Me" onClick={(e) => Meteor.call('admin.sendReport')}/>
+        </Form.Group>
+
+        <Form.Group>
+          <Form.Button icon="mail" content="Email List of Users & Teams to Me" onClick={(e) => { Meteor.call('admin.sendUsersAndTeams'); alert("Emails are sending!"); }}/>
+        </Form.Group>
+
+        <Header as="h4" content="Nightly Reports" />
+        <Form.Group>
+          {this.state.sendReportsTo.map(email => {
+            return (
+              <Form.Button
+                type="button"
+                onClick={(e) => this.removeRecipient(e, email)}
+                key={email}
+                content={email}
+                icon="x"
+                color="red"
+                size="tiny" />
+            );
+          })}
+        </Form.Group>
+        <Form.Group>
+          <Form.Input
+            placeholder="you@example.com"
+            name="reportEmail"
+            value={this.state.reportEmail}
+            onChange={this.handleChange}
+            />
+
+          <Form.Button
+            content="Add recipient to reports list"
+            onClick={(e) => this.addRecipient(e, this.state.reportEmail)} />
+        </Form.Group>
         <Form.Group>
           { this._fieldButton('doSendNightlyReports', "Nightly Reports") }
         </Form.Group>
@@ -101,10 +134,34 @@ class GamestateControlsInner extends Component {
     return (
       <Form.Button icon="download"
         content={name}
-        onClick={ ()=> this._getReport(index) }
+        onClick={ (e)=> this._getReport(e, index) }
         disabled={this.state.reportDLBusy} />
     );
   }
+
+  removeRecipient(e, email) {
+    e.preventDefault();
+    if(confirm(`Are you sure you want to remove ${email} from nightly reports?`)){
+      Meteor.call(`admin.gamestate.reports.removeRecipient`, email, error => {
+        if (error){
+          console.log(error);
+          alert("Failed to remove email. " + error.reason);
+        }
+      });
+    }
+  }
+
+  addRecipient(e, email) {
+    e.preventDefault();
+    if(!email) return;
+
+    Meteor.call(`admin.gamestate.reports.addRecipient`, email, error => {
+      if (error){
+        console.log(error);
+        alert("Failed to add email. " + error.reason);
+      }
+    });
+  };
 
   _fieldButton(fieldName, displayName) {
     if(!displayName){
