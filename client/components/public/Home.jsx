e772bc13c14e21df3cf51e3ae1da5a8cc5f3ee3d
cabeese <Noah.Strong+github@outlook.com>
2021-04-16 17:41:52 -0500

    Home: add gameplay mechanics link to announcements banner

--- client/components/public/Home.jsx
+++ client/components/public/Home.jsx
@@ -27,8 +27,12 @@ const AnnouncementsMessage = (
                     </Message.Header>
                     <Segment basic size='large' className='no-padding' style={{lineHeight: "normal"}}>
                     <ul>
-                      <li><strong>IMPORTANT</strong>: Game day is soon! Make sure at least one team member can obtain the <a target="_blank" href="https://gph-distributed.s3-us-west-2.amazonaws.com/GPH2021-what-you-need.pdf">necessary materials</a>.</li>
-					  <li>Check-in starts at <strong>9:30 am Pacific Time.</strong> See <a target="_blank" href="https://www.timeanddate.com/worldclock/converter.html?iso=20210417T163000&p1=2447&p2=179&p3=136&p4=16&p5=240">other timezones.</a></li>
+                      <li><strong>IMPORTANT</strong>: Game day is soon! Make sure at least one team member can obtain the <a target="_blank" href="https://gph-distributed.s3-us-west-2.amazonaws.com/GPH2021-what-you-need.pdf">necessary materials (PDF)</a>.</li>
+                      <li>Check-in starts at <strong>9:30 am Pacific Time.</strong> See <a target="_blank" href="https://www.timeanddate.com/worldclock/converter.html?iso=20210417T163000&p1=2447&p2=179&p3=136&p4=16&p5=240">other timezones.</a></li>
+                      <li>
+                        To understand how check-in and gameplay work, check out the <a target="_blank" href="https://gph-distributed.s3-us-west-2.amazonaws.com/GPH2021-game-mechanics.pdf">
+                        gameplay mechanics (PDF)</a>.
+                      </li>
                       <li>The WWU Fifth Annual Great Puzzle Hunt is virtual, free, fun, and safe (See Safety <a href="/faq">FAQ</a>).</li>
                     </ul>
                     <br/>
