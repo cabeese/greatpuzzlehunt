170697c56eba0628de61d43853d2c5d743fdd448
richardgolding <golding@chrysaetos.org>
2021-12-27 10:04:03 -0800

    checkpoint

--- client/components/looking-for-team/imports/lft-user-list.jsx
+++ client/components/looking-for-team/imports/lft-user-list.jsx
@@ -2,17 +2,30 @@ import { Meteor } from 'meteor/meteor';
 import { withTracker } from 'meteor/react-meteor-data';
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Card, Icon } from 'semantic-ui-react';
+import { Card, Icon, Button } from 'semantic-ui-react';
+
+import MessageUserModal from '../../message-user/message-user';
 
 class Users extends Component {
+    constructor(props) {
+	super(props);
+	this.setState( { } );
+	// Pick up here: need to work out what state to create, and then copy the pattern
+	// from AdminUserTable to manage the messageUser state variable that will turn the
+	// modal dialog off and on
+    }
+    
   render() {
     if (!this.props.ready) return <Loading/>
 
-    return (
-      <Card.Group>
-        { this._users() }
-      </Card.Group>
-    );
+      return (
+	  <div>
+	      <Card.Group>
+		  { this._users() }
+	      </Card.Group>
+	      <MessageUserModal user={messageUser} clearUser={() => this._clearMessageUser()}/>
+	  </div>
+      );
   }
 
   _users() {
@@ -27,11 +40,15 @@ class Users extends Component {
           </Card.Meta>
         </Card.Content>
         <Card.Content extra>
-          <Icon name='mail'/> <a href={`mailto:${user.getEmail()}`}>{user.getEmail()}</a>
+            <Button basic icon='mail' content='Message player' onClick={() => this._messageUser(user)}/>
         </Card.Content>
       </Card>
     ));
   }
+
+    _messageUser(user) {
+	console.log('message user: ', user);
+    }
 }
 
 Users = withTracker(() => {
