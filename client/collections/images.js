0bb4bf715c9a6462f0518db95e46e7f5b4a9cd09
RvanB <rvanbronkhorst@gmail.com>
2022-01-27 20:52:55 -0800

    Disable image store

--- client/collections/images.js
+++ client/collections/images.js
@@ -1,23 +1,23 @@
 import { Meteor } from 'meteor/meteor';
 import { isAdmin } from '../../lib/imports/method-helpers';
 
-const store = new FS.Store.GridFS('images');
+// const store = new FS.Store.GridFS('images');
 
-Images = new FS.Collection("images", {
-  stores: [store],
-});
+// Images = new FS.Collection("images", {
+//   stores: [store],
+// });
 
-Images.allow({
-  insert(userId, fileObj) {
-    return isAdmin(userId);
-  },
-  update(userId, fileObj) {
-    return isAdmin(userId);
-  },
-  download(userId, obj) {
-    return true;
-  },
-  remove(userId, fileObj) {
-    return isAdmin(userId);
-  }
-});
+// Images.allow({
+//   insert(userId, fileObj) {
+//     return isAdmin(userId);
+//   },
+//   update(userId, fileObj) {
+//     return isAdmin(userId);
+//   },
+//   download(userId, obj) {
+//     return true;
+//   },
+//   remove(userId, fileObj) {
+//     return isAdmin(userId);
+//   }
+// });
