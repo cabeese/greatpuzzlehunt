32113ae533cb808ed8b6bae47deeac60f9cf4224
RvanB <rvanbronkhorst@gmail.com>
2022-05-19 21:56:30 -0700

    Add filters to users page

--- client/components/admin/AdminUsers/AdminUsers.jsx
+++ client/components/admin/AdminUsers/AdminUsers.jsx
@@ -1,6 +1,6 @@
 import { Meteor } from 'meteor/meteor';
 import React, { Component } from 'react';
-import { Grid, Container, Input, Menu, Icon, Label } from 'semantic-ui-react';
+import { Grid, Container, Input, Menu, Icon, Label, Checkbox } from 'semantic-ui-react';
 
 import DebounceSearch from '../../imports/DebounceSearch';
 import AdminUserListTracker from './imports/AdminUserListTracker';
@@ -11,18 +11,66 @@ AdminUsers = class AdminUsers extends Component {
     this.state = {
       userSearch: '',
       teamSearch: '',
+      userFilter: {
+        verified: {
+          value: false,
+          applied: false
+        },
+        paid: {
+          value: false,
+          applied: false
+        }
+      },
     };
   }
 
+  _filterCheckbox(key, label) {
+    return (
+      <>
+      <Checkbox
+        label={label}
+        onChange={(e, data) => {
+          let copy = { ...this.state.userFilter };
+          let key_copy = { ...this.state.userFilter[key] };
+          key_copy.value = data.checked;
+
+          copy[key] = key_copy
+
+          this.setState({ userFilter: copy });
+        }}
+      />
+      <Checkbox
+        label={'Apply ' + key}
+        onChange={(e, data) => {
+          let copy = { ...this.state.userFilter };
+          let key_copy = { ...this.state.userFilter[key] };
+          key_copy.applied = data.checked;
+
+          copy[key] = key_copy
+
+          this.setState({ userFilter: copy });
+        }}
+      />
+      </>
+    )
+  }
+
   render() {
-    const { userSearch, teamSearch } = this.state;
+    const { userSearch, teamSearch, userFilter } = this.state;
 
     return (
       <Container>
         <PuzzlePageTitle title='Admin: Users'/>
 
+        { this._filterCheckbox('verified', 'Email verified') }
+        <br />
+        { this._filterCheckbox('paid', 'Paid') }
+        <br />
+        { this._filterCheckbox('volunteer', 'Volunteer') }
+        <br />
+        { this._filterCheckbox('onteam', 'On a team') }
+        <br /><br />
         <Grid stackable>
-
           <Grid.Row columns={2}>
             <Grid.Column width={10}>
               <DebounceSearch
@@ -46,7 +94,7 @@ AdminUsers = class AdminUsers extends Component {
 
           <Grid.Row>
             <Grid.Column>
-              <AdminUserListTracker userSearch={userSearch} teamSearch={teamSearch}/>
+              <AdminUserListTracker userSearch={userSearch} teamSearch={teamSearch} userFilter={userFilter}/>
             </Grid.Column>
           </Grid.Row>
 
--- client/components/admin/AdminUsers/imports/AdminUserListTracker.jsx
+++ client/components/admin/AdminUsers/imports/AdminUserListTracker.jsx
@@ -4,7 +4,7 @@ import React, { Component } from 'react';
 
 import AdminUserTable from './AdminUserTable';
 
-export default AdminUserListTracker = withTracker(({ userSearch, teamSearch }) => {
+export default AdminUserListTracker = withTracker(({ userSearch, teamSearch, userFilter }) => {
   const usersHandle = Meteor.subscribe('admin.users', userSearch);
   const loading = !usersHandle.ready();
 
@@ -48,6 +48,34 @@ export default AdminUserListTracker = withTracker(({ userSearch, teamSearch }) =
   if (teamSearch && teamSearch.length > 0) {
     const teamSearchSafe = teamSearch.trim().toLowerCase();
     users = users.filter((user, i, _users) => (user.teamName.toLowerCase().search(teamSearchSafe) > -1));
+  }
+  
+  // Filter users by userFilter
+  for (let key in userFilter) {
+    filter = userFilter[key]
+
+    if (filter.applied) {
+      users = users.filter((user, i, _users) => {
+        let result = false;
+        switch(key) {
+          case 'verified':
+            result = user.isVerified();
+            break;
+          case 'paid':
+            result = user.paid
+            break;
+          case 'volunteer':
+            result = user.hasRole('volunteer');
+            break;
+          case 'onteam':
+            // TODO: !! probably not best way to get boolean
+            result = !!user.teamId;
+            break;
+        }
+
+        return result == filter.value;
+      })
+    }
   }
 
   return {
