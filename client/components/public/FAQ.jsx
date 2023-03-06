a40cf4b78e90f6071fd50f53a893aade25d104a1
Richard Golding <golding@chrysaetos.org>
2023-03-05 19:26:45 -0800

    Fix language to remove reference to ticket codes

--- client/components/public/FAQ.jsx
+++ client/components/public/FAQ.jsx
@@ -63,7 +63,7 @@ const importantDates = (
     {/* <List.Item><strong>{earlyBirdLastDate}</strong>: Early Bird discount prices for ticket codes and official gear end.</List.Item> */}
     <List.Item><strong>{regularRegistrationStart}</strong>: Registration and Official Gear Store opens</List.Item>
     <List.Item><strong>{registrationCloseDate}</strong>: Step 1 of Registration (Create an Account) Closes - Or earlier if team limit is reached</List.Item>
-    <List.Item><strong>{eventDate}</strong>: If you've already created an account, you can acquire, redeem ticket code(s), and join a team until 10:00 AM (PT).</List.Item>
+    <List.Item><strong>{eventDate}</strong>: If you've already created an account, you can join a team until 10:00 AM (PT).</List.Item>
     <List.Item><strong>{gearSaleEnd}</strong>: Official Gear store closes.</List.Item>
   </List>
 );
