cbb4b86213b06d476834e5ddc82a5a2bd97ef66e
Richard Golding <golding@chrysaetos.org>
2024-09-06 17:54:47 -0700

    Add method and subscription rate limiters

--- server/imports/start-up.js
+++ server/imports/start-up.js
@@ -4,6 +4,7 @@
 */
 import { cloneDeep } from 'lodash';
 import { WebApp} from 'meteor/webapp';
+import { DDPRateLimiter } from 'meteor/ddp-rate-limiter';
 
 Meteor.startup(() => {
 
@@ -33,5 +34,28 @@ Meteor.startup(() => {
   WebApp.connectHandlers.use(function (req, res, next) {
     res.setHeader('Strict-Transport-Security', 'max-age=86400; includeSubDomains');
     next();
+  });
+
+  // set up rate limiting for DDP methods and subscriptions
+  // This rule allows ten method calls of any kind from one IP address
+  // per second
+  const mruleid = DDPRateLimiter.addRule({
+    type: 'method',
+    clientAddress() { return true; }
+  }, 10, 1000, function (data, err) {
+    if (!data.allowed) {
+    Meteor.logger.info('Method rate limiter rule invoked for client at ' + err.clientAddress + ' method ' + err.name);
+    }
+  });
+
+  // This rule allows 1000 subscription calls of any kind from one IP
+  // address per second
+  const sruleid = DDPRateLimiter.addRule({
+    type: 'subscription',
+    clientAddress() { return true; }
+  }, 100, 1000, function (data, err) {
+    if (!data.allowed) {
+      Meteor.logger.info('Subscription rate limiter rule invoked for client at ' + err.clientAddress + ' subscription ' + err.name);
+    }
   });
 });
