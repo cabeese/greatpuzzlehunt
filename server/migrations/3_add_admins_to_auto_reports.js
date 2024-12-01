bcdc984b5958bb0a9a911c18934b461928ade6de
cabeese <Noah.Strong+github@outlook.com>
2024-11-30 18:56:56 -0600

    FIXUP for migrations

--- server/migrations/3_add_admins_to_auto_reports.js
+++ server/migrations/3_add_admins_to_auto_reports.js
@@ -1,6 +1,9 @@
 import { Meteor } from 'meteor/meteor';
 import { Migrations } from "meteor/percolate:migrations";
 
+// NOTE! Migrations are no longer run in sequence!
+// This is due to the Meteor 3 transition to async/await.
+// See https://github.com/percolatestudio/meteor-migrations/issues/89
 Migrations.add({
   version: 3,
   up: function () {
