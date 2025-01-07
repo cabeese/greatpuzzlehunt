77ecb019e3868da71de5ee71b931fa05850c94e6
cabeese <Noah.Strong+github@outlook.com>
2025-01-06 20:35:36 -0600

    Update Migrations

--- server/migrations/3_add_admins_to_auto_reports.js
+++ server/migrations/3_add_admins_to_auto_reports.js
@@ -1,9 +1,16 @@
 import { Meteor } from 'meteor/meteor';
+import { Mongo } from 'meteor/mongo';
+import { Migrations } from "meteor/percolate:migrations";
 
+import { Gamestate } from '../../lib/collections/gamestate-collection.js'
+
+// NOTE! Migrations are no longer run in sequence!
+// This is due to the Meteor 3 transition to async/await.
+// See https://github.com/percolatestudio/meteor-migrations/issues/89
 Migrations.add({
   version: 3,
-  up: function () {
-    Gamestate.update({}, { $set: {
+  up: async function () {
+    await Gamestate.updateAsync({}, { $set: {
       sendReportsTo: ['greatpuzzlehunt@gmail.com', 'milliejohnson3.14@gmail.com'],
     }});
   },
