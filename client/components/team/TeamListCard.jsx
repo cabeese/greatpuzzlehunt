431f62c10e8cc43416aca16d6b8f1bbdf4d90ecf
Richard Golding <golding@chrysaetos.org>
2025-01-08 21:06:50 -0800

    FAQ and gear changes

--- client/components/team/TeamListCard.jsx
+++ client/components/team/TeamListCard.jsx
@@ -80,7 +80,7 @@ TeamListCard = class TeamListCard extends Component {
     if (count >= 6) return 'blue';
     else if (count >= 4) return 'green';
     else return 'orange';
-  }
+  } 
 
   _getCardExtra() {
     if (this.props.public) {
