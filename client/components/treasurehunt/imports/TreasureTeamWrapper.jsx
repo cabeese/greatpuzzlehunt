baa65d7e70e9f413f78e4e1b4dac7909c421be8e
Richard Golding <golding@chrysaetos.org>
2025-12-23 20:59:11 -0800

    start team playing treasure hunt

--- client/components/treasurehunt/imports/TreasureTeamWrapper.jsx
+++ client/components/treasurehunt/imports/TreasureTeamWrapper.jsx
@@ -2,51 +2,139 @@ import { Meteor } from 'meteor/meteor';
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-import { Container, Message, Button } from 'semantic-ui-react';
+import { Button, Container, Grid, Header, Label, Message } from 'semantic-ui-react';
 
 import TeamComp from '../../imports/TeamComp';
 // import GameUI from './GameUI.jsx';
 
 class TreasureTeamWrapper extends Component {
   render() {
+    const { ready } = this.props;
     return (
       <Container>
         {this._title()}
-        {this._content()}
+	{ ready ? this._content() : this._loading() }
       </Container>
     );
   }
 
   _title() {
-    const { ready, team } = this.props;
-    return <PuzzlePageTitle title={team ? team.name : 'The Game'} />;
+    return ( <PuzzlePageTitle title='Treasure Hunt status' /> );
+  }
+
+  _loading() {
+    return <Loading/>;
   }
 
   _content() {
-    const { ready, user, team } = this.props;
-    if (!ready) {
-      return <Loading/>;
-    }
-
+    const { user, team } = this.props;
     if (!team) {
       return <NoTeamMessage/>;
+    } else {
+      return (
+	<div>
+	  { this._subtitle() }
+	  { this._checkinStatus() }
+	  { this._huntStatus() }
+	</div>
+      );
     }
-
-    if (!team.checkinConfirmed) {
-      return this._checkin();
-    }
-
-    return 'UI content here'
-    return <GameUI team={team} user={user} />
   }
 
-  _checkin() {
-    return (
-      <Message info size="large">
-        <Message.Header>You need to check in!</Message.Header>
-        <Message.Content><p></p><Link to="/team/checkin"><Button color="green" content="Check In"/></Link></Message.Content>
-      </Message>
-    );
+  _subtitle() {
+    const { team } = this.props;
+  }
+
+  _checkinStatus() {
+    const { team } = this.props;
+    if (team) {
+      if (team.startedTreasureHunt) {
+	return (
+	  <Message info>
+	    <Message.Header>
+	      Team { team.name } has started playing
+	    </Message.Header>
+	    <Button basic size='small' content='Clear playing'
+		    color='black'
+		    onClick={ async () => await this._clearPlaying() }
+		    />
+	  </Message>
+	);
+      } else {
+	return (
+	  <Grid.Row columns='1'>
+	    <Container>
+	      Team { team.name } has not yet started playing
+	    </Container>
+	    <Button basic size='small' content='Start playing'
+		    color='black'
+		    onClick={ async () => await this._startPlaying() }
+		    />
+	  </Grid.Row>
+	);
+      }
+    } else {
+      return (
+	<Message warning size="large">
+	  <Message.Header>
+	    No team!
+	  </Message.Header>
+	</Message>
+      );
+    }
+  }
+
+  _huntStatus() {
+    const { team } = this.props;
+    if (team) {
+      if (team.startedTreasureHunt) {
+	return (
+	  <Grid stackable>
+	    <Grid.Row columns='1'>
+	      <Header content='Checkpoints' />
+	    </Grid.Row>
+	  </Grid>
+	);
+      } else {
+	return null;
+      }
+    } else {
+      return (
+	<Message warning size="large">
+	  <Message.Header>
+	    No team!
+	  </Message.Header>
+	</Message>
+      );
+    }
+  }
+
+  async _startPlaying() {
+    const { team } = this.props;
+    const teamId = team._id;
+    console.log('Start playing!');
+    console.log(teamId);
+    try {
+      await Meteor.callAsync('team.startTreasureHunt', teamId);
+      console.log('back from start TH');
+    } catch (error) {
+      console.log(error);
+      alert(error.reason);
+    }
+  }
+
+  async _clearPlaying() {
+    const { team } = this.props;
+    const teamId = team._id;
+    console.log('clear playing!');
+    console.log(teamId);
+    try {
+      await Meteor.callAsync('team.clearTreasureHunt', teamId);
+      console.log('back from clear TH');
+    } catch (error) {
+      console.log(error);
+      alert(error.reason);
+    }
   }
 }
 
