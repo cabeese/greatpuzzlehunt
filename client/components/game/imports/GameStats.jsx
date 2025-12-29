aea44b29fb17566e131db7523152cadfd10a18c3
Richard Golding <golding@chrysaetos.org>
2025-12-29 12:30:10 -0800

    When done with puzzle hunt, show link to treasure hunt

--- client/components/game/imports/GameStats.jsx
+++ client/components/game/imports/GameStats.jsx
@@ -1,7 +1,9 @@
 import { Meteor } from 'meteor/meteor';
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Segment, Message, Header, Statistic, Progress } from 'semantic-ui-react';
+import { Button, Header, Message, Progress, Segment, Statistic } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+import { TreasureHuntIcon } from '../../imports/PuzzleTreasureIcons';
 
 import { renderScore } from '../../imports/PuzzleProgress';
 import { getFinalScore } from '../../../../lib/imports/puzzle-helpers';
@@ -24,6 +26,8 @@ class GameStats extends Component {
 
   render() {
     const { team } = this.props;
+    console.log('team game play:');
+    console.log(team);
     const puzzlesSolved = team.puzzles.reduce((acc, p) => (acc + (p.score ? 1 : 0)), 0);
     const finished = team.puzzles.length === puzzlesSolved;
     const { time, minutes } = renderScore(getFinalScore(team));
@@ -42,6 +46,7 @@ class GameStats extends Component {
         </p>
 
         { this._doneMessage(finished) }
+	{ this._treasureMessage(finished, team.playingTreasureHunt) }
       </Message>
     );
   }
@@ -53,6 +58,26 @@ class GameStats extends Component {
         content={`Congratulations! You've finished the ${eventYear} Puzzle Hunt!`}
         subheader="Tune into the webcast or return to Red Square by 5pm PT for prizes."
       />
+    );
+  }
+
+  _treasureMessage(finished, playing) {
+    if (!finished) {
+      return null;
+    }
+    if (!playing) {
+      return null;
+    }
+    return (
+      <div>
+	<TreasureHuntIcon value={true} />
+	&nbsp;
+	You're signed up to play the Treasure Hunt! When you're ready, head to the
+	&nbsp;
+	<Link to='/treasure'>
+	  <Button content='Team status page' />
+	</Link>
+      </div>
     );
   }
 
